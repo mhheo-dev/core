1f1c5a8d1e19b87a0de1bc6be23d9e8611599821
mhheo <mhheo.dev@gmail.com>
2023-08-15 23:00:39 +0900

    Chore: DIP 문제점 있는 코드 부 주석

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // 추상화 및 구체화에 둘다 의존 DIP 위배 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
