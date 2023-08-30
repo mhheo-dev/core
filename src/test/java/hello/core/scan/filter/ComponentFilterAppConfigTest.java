11e1867106ecbcfc97229cbb7c1cb2fd52297f12
mhheo <mhheo.dev@gmail.com>
2023-08-30 21:39:27 +0900

    Feat: 스프링부트에서는 기본값 bean의 ID 가 중복이 불가능함

--- src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -2,17 +2,14 @@ package hello.core.scan.filter;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.logging.Filter;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.stereotype.Component;
 
 public class ComponentFilterAppConfigTest {
 
