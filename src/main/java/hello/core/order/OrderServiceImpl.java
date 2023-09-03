39c5f4879e7468bffef11b3a71faa36653c1feb0
mhheo <mhheo.dev@gmail.com>
2023-09-03 23:06:50 +0900

    Test: 의존관계 주입방식
     1) 생성자 주입방식
     1-1) 생성자 직접 사용방식
     1-2) Setter 사용방식
     2) 직접주입방식: 가급적 사용X, Config에서 사용
     3) 일반메서드 주입방식: 잘사용 X

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,11 +9,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    /* 직접 주입방식 테스트가 어려움 , 권장 x */
+    //    @Autowired
     private final MemberRepository memberRepository;
+    //    @Autowired
     private final DiscountPolicy discountPolicy;
 
+    //    @Autowired
+    //    public void setMemberRepository(MemberRepository memberRepository) {
+    //        System.out.println("memberRepository = " + memberRepository);
+    //        this.memberRepository = memberRepository;
+    //    }
+    //
+    //    @Autowired
+    //    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+    //        System.out.println("discountPolicy = " + discountPolicy);
+    //        this.discountPolicy = discountPolicy;
+    //    }
+
+    //순서
+    // 1.OrderServiceImpl 생성자
+    // 2.setter를 이용한 것 setMember(setter 사용시 아래 생성자 필요 x, 의존성 다주입해줌)
+
+    /*생성자가 한개일 경우 Autowired 생략가능*/
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("orderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
